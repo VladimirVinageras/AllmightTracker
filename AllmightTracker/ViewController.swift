85458ed8a7a91bb1a3863800d97853c05840ed77
Vladimir Vinakheras <vvinageras@gmail.com>
2024-05-04 00:39:28 +0200

    Initial Commit

---
+++ AllmightTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AllmightTracker
+//
+//  Created by Vladimir Vinakheras on 04.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
