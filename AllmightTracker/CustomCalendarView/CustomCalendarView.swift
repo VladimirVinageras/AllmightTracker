9a4fc5766c1e93554a4b2f69b689eddaae512594
Vladimir Vinakheras <vvinageras@gmail.com>
2024-05-23 01:11:35 +0200

    CustomCalendarCreated... saving project

---
+++ AllmightTracker/CustomCalendarView/CustomCalendarView.swift
@@ -0,0 +1,95 @@
+//
+//  CustomCalendarView.swift
+//  AllmightTracker
+//
+//  Created by Vladimir Vinakheras on 23.05.2024.
+//
+
+import Foundation
+import UIKit
+
+class CustomCalendarView: UIView {
+    
+    var onDateSelected: ((Date) -> Void)?
+    
+    private let calendar = Calendar.current
+    private var selectedDate = Date()
+    
+    private let calendarCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(CalendarDateCell.self, forCellWithReuseIdentifier: "customsCalendarCollectionViewCell")
+        collectionView.backgroundColor = .trackerWhite
+        return collectionView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        activateConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        addSubview(calendarCollectionView)
+        calendarCollectionView.delegate = self
+        calendarCollectionView.dataSource = self
+        
+        
+    }
+    
+    private func activateConstraints(){
+        NSLayoutConstraint.activate([
+            calendarCollectionView.topAnchor.constraint(equalTo: topAnchor),
+            calendarCollectionView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            calendarCollectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            calendarCollectionView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+    
+    private func dateForCell(at indexPath: IndexPath) -> Date {
+        let startOfMonth = calendar.date(from: calendar.dateComponents([.year, .month], from: selectedDate))!
+        let weekday = calendar.component(.weekday, from: startOfMonth) - calendar.firstWeekday
+        let daysOffset = (indexPath.item - weekday + 7) % 7
+        return calendar.date(byAdding: .day, value: daysOffset, to: startOfMonth)!
+    }
+}
+extension CustomCalendarView : UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 42
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "customsCalendarCollectionViewCell", for: indexPath) as? CalendarDateCell else {return CalendarDateCell()}
+        cell.backgroundColor = .lightGray
+        let date = dateForCell(at: indexPath)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "d"
+        cell.dateLabel.text = dateFormatter.string(from: date)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let date = Date()
+        onDateSelected?(date)
+    }
+}
+
+extension CustomCalendarView : UICollectionViewDelegate, UICollectionViewDelegateFlowLayout{
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width / 7
+        let height = collectionView.frame.height / 6
+        return CGSize(width: width, height: height)
+    }
+}
+
--- AllmightTracker/Screens/MainScreen(TabBar+Trackers+Stats)/TrackersViewController.swift
+++ AllmightTracker/Screens/MainScreen(TabBar+Trackers+Stats)/TrackersViewController.swift
@@ -11,22 +11,43 @@ import UIKit
 final class TrackersViewController : UIViewController {
     var categories: [TrackerCategory] = []
     var completedTrackers: [TrackerRecord] = []
+
+    private let trackerDateLabel: UILabel = {
+        let label = UILabel()
+        label.layer.frame = CGRect(x: 0, y: 0, width: 77, height: 34)
+        label.layer.backgroundColor = UIColor.trackerLightGray.cgColor
+        label.layer.cornerRadius = 8
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 17)
+        label.isUserInteractionEnabled = true
+
+        return label
+    }()
     
-    private let addTrackerButton = UIButton()
     private lazy var trackerDatePicker : UIDatePicker = {
         let datePicker = UIDatePicker()
-        datePicker.preferredDatePickerStyle = .compact
+        datePicker.layer.backgroundColor = UIColor.trackerWhite.cgColor
+        if #available(iOS 14.0, *) {
+            datePicker.preferredDatePickerStyle = .inline
+        } else {
+            datePicker.datePickerMode = .date
+        }
         datePicker.datePickerMode = .date
-        datePicker.locale = Locale(identifier: "ru_Ru")
+        datePicker.locale = Locale(identifier: "ru_RU")
         datePicker.calendar.firstWeekday = 2
         datePicker.translatesAutoresizingMaskIntoConstraints = false
-        datePicker.clipsToBounds = true
-        datePicker.layer.cornerRadius = 8
-        datePicker.tintColor = .trackerBlue
-        datePicker.addTarget(self, action: #selector(dateValueChanged(_:)), for: .valueChanged)
-        
+        datePicker.layer.cornerRadius = 13
+        datePicker.layer.shadowColor = UIColor.lightGray.cgColor
+        datePicker.layer.shadowOpacity = 0.2
+        datePicker.layer.shadowOffset = CGSize(width: 0, height: 0)
+        datePicker.layer.shadowRadius = 10
         return datePicker
     }()
+    
+    
+    private let addTrackerButton = UIButton()
+    
+    
     
     private let trackerSearchField = UISearchTextField()
     private let starImageView = UIImageView()
@@ -38,44 +59,56 @@ final class TrackersViewController : UIViewController {
         view.backgroundColor = .trackerWhite
         prepareNavigationBar()
         prepareStarMainScreen()
-     //   dateValueChanged(_:)
+        prepareDateUIItems()
         activateConstraints()
     }
+    func prepareDateUIItems(){
+        let currentDate = Date()
+        updateLabel(with: currentDate)
+        view.addSubview(trackerDatePicker)
+        trackerDatePicker.addTarget(self, action: #selector(dateValueChanged(_:)), for: .valueChanged)
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dateLabelTapped))
+        trackerDateLabel.addGestureRecognizer(tapGesture)
+        trackerDatePicker.isHidden = true
+    }
     
+    @objc func dateLabelTapped() {
+        trackerDatePicker.isHidden = false
+        view.bringSubviewToFront(trackerDatePicker)
+    }
+    
+    @objc func dateValueChanged(_ sender: UIDatePicker) {
+        updateLabel(with: sender.date)
+        trackerDatePicker.isHidden = true
+        view.sendSubviewToBack(trackerDatePicker)
+    }
+    
+    func updateLabel(with date: Date) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yy"
+        trackerDateLabel.text = dateFormatter.string(from: date)
+    }
     
     private func prepareNavigationBar(){
        
         addTrackerButton.setImage(UIImage(named: "navBarAddIcon"), for: .normal)
         addTrackerButton.translatesAutoresizingMaskIntoConstraints = false
         addTrackerButton.addTarget(self, action: #selector(plusButtonTapped), for: .touchUpInside)
-        
-     
-       
-        
-        
-        
+    
         screenTitle.translatesAutoresizingMaskIntoConstraints = false
         screenTitle.text = "Трекеры"
         screenTitle.textColor = .trackerBlack
         screenTitle.font = UIFont.systemFont(ofSize: 34, weight: .bold)
-        
-        
+
         trackerSearchField.translatesAutoresizingMaskIntoConstraints = false
         trackerSearchField.placeholder = "Поиск"
         
         view.addSubview(trackerSearchField)
         view.addSubview(screenTitle)
         navigationItem.leftBarButtonItem = UIBarButtonItem(customView: addTrackerButton)
-        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: trackerDatePicker)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: trackerDateLabel)
     }
-    
-    @objc private func dateValueChanged(_ sender: UIDatePicker){
-        let selectedDate = sender.date
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd.MM.yy"
-        let formattedDate = dateFormatter.string(from: selectedDate)
-    }
-    
+        
     @objc private func plusButtonTapped() {
    present(AddNewTrackerViewController(), animated: true)
     }
@@ -87,7 +120,6 @@ final class TrackersViewController : UIViewController {
         starImageView.image = UIImage(named: "StarMainScreen")
         starImageView.frame.size = CGSize(width: 80, height: 80)
         
-       
         starLabel.translatesAutoresizingMaskIntoConstraints = false
         starLabel.text = "Что будем отслеживать?"
         starLabel.textColor = .trackerBlack
@@ -109,7 +141,10 @@ final class TrackersViewController : UIViewController {
             starImageView.heightAnchor.constraint(equalToConstant: 80),
             starImageView.widthAnchor.constraint(equalToConstant: 80),
             starLabel.centerXAnchor.constraint(equalTo: starImageView.centerXAnchor),
-            starLabel.topAnchor.constraint(equalTo: starImageView.bottomAnchor, constant: 8)
+            starLabel.topAnchor.constraint(equalTo: starImageView.bottomAnchor, constant: 8),
+            trackerDatePicker.topAnchor.constraint(equalTo: screenTitle.bottomAnchor, constant: 7),
+            trackerDatePicker.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            trackerDatePicker.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
         ])
     }
 
