d7e9cbb39485a8aab4cdcf7e5317ae5d713ae48c
Vladimir Vinakheras <vvinageras@gmail.com>
2024-06-11 18:38:59 +0200

    fixing coredata issue

---
+++ Build/Intermediates.noindex/AllmightTracker.build/Debug-iphonesimulator/AllmightTracker.build/DerivedSources/CoreDataGenerated/ModelCoreData/TrackerCoreData+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  TrackerCoreData+CoreDataClass.swift
+//  
+//
+//  Created by Vladimir Vinakheras on 11.06.2024.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(TrackerCoreData)
+public class TrackerCoreData: NSManagedObject {
+
+}
---
+++ Build/Intermediates.noindex/AllmightTracker.build/Debug-iphonesimulator/AllmightTracker.build/DerivedSources/CoreDataGenerated/ModelCoreData/TrackerScheduleCoreData+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  TrackerScheduleCoreData+CoreDataClass.swift
+//  
+//
+//  Created by Vladimir Vinakheras on 11.06.2024.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(TrackerScheduleCoreData)
+public class TrackerScheduleCoreData: NSManagedObject {
+
+}
