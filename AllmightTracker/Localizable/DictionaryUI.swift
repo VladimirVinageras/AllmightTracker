45fe8e8f4a7d74a685d05f753e6d36638a6de941
Vladimir Vinakheras <vvinageras@gmail.com>
2024-06-23 04:24:30 +0200

    Localization Done

---
+++ AllmightTracker/Localizable/DictionaryUI.swift
@@ -0,0 +1,53 @@
+//
+//  DictionaryUI.swift
+//  AllmightTracker
+//
+//  Created by Vladimir Vinakheras on 22.06.2024.
+//
+
+import Foundation
+
+struct DictionaryUI {
+   let onboardingViewTextPage1  = NSLocalizedString("onboardingView.TextPage1", comment: "")
+   let onboardingViewTextPage2 = NSLocalizedString("onboardingView.TextPage2", comment: "")
+   
+    let onboardingViewBtnAction = NSLocalizedString("onboardingView.BtnAction", comment: "")
+    
+    let tabBarTrackers = NSLocalizedString("tabBar.trackers", comment: "")
+    let tabBarStatistics = NSLocalizedString("tabBar.statistics", comment: "")
+    let trackersViewTitle = NSLocalizedString("trackersView.title", comment: "")
+    let addNewTrackerViewCreateTrackerTitle = NSLocalizedString("addNewTrackerView.createTrackerTitle", comment: "")
+    let addNewTrackerViewHabit = NSLocalizedString("addNewTrackerView.habit", comment: "")
+    let addNewTrackerViewOnceTimeEvent = NSLocalizedString("addNewTrackerView.onceTimeEvent", comment: "")
+    let createHabitViewTitle = NSLocalizedString("createHabitView.title", comment: "")
+    let createHabtiViewCellCategory = NSLocalizedString("createHabtiView.cellCategory", comment: "")
+    let createHabtiViewCellSchedule = NSLocalizedString("createHabtiView.cellSchedule", comment: "")
+    let createHabtiViewEmojisCollectionTitle = NSLocalizedString("createHabtiView.emojisCollectionTitle", comment: "")
+    let createHabtiViewColorCollectionTitle = NSLocalizedString("createHabtiView.colorCollectionTitle", comment: "")
+    let createHabtiViewBtnCancelTitle = NSLocalizedString("createHabtiView.btnCancelTitle", comment: "")
+    let createHabtiViewBtnCreateTitle = NSLocalizedString("createHabtiView.btnCreateTitle", comment: "")
+    let createHabtiViewTextFieldHolderText = NSLocalizedString("createHabtiView.textFieldHolderText", comment: "")
+    let createHabtiViewEveryDayHabitText = NSLocalizedString("createHabtiView.everyDayHabit", comment: "")
+    let addCategoryViewTitle = NSLocalizedString("addCategoryView.title", comment: "")
+    let addCategoryViewBtnAddCategory = NSLocalizedString("addCategoryView.btnAddCategory", comment: "")
+    let createCategoryTitle = NSLocalizedString("createCategory.title", comment: "")
+    let createCategoryTextFieldHolderText = NSLocalizedString("createCategory.textFieldHolderText", comment: "")
+    let createCategoryBtnReady = NSLocalizedString("createCategory.btnReady", comment: "")
+    let addNewScheduleTitle = NSLocalizedString("addNewSchedule.title", comment: "")
+    let addNewScheduleBtnReady = NSLocalizedString("addNewSchedule.btnReady", comment: "")
+    let addNewScheduleMonday = NSLocalizedString("addNewSchedule.monday", comment: "")
+    let addNewScheduleThuesday = NSLocalizedString("addNewSchedule.thuesday", comment: "")
+    let addNewScheduleWednesday = NSLocalizedString("addNewSchedule.wednesday", comment: "")
+    let addNewScheduleThursday = NSLocalizedString("addNewSchedule.thursday", comment: "")
+    let addNewScheduleFriday = NSLocalizedString("addNewSchedule.friday", comment: "")
+    let addNewScheduleSaturday = NSLocalizedString("addNewSchedule.saturday", comment: "")
+    let addNewScheduleSunday = NSLocalizedString("addNewSchedule.sunday", comment: "")
+    let addNewScheduleShortMonday = NSLocalizedString("addNewSchedule.shortMonday", comment: "")
+    let addNewScheduleShortThuesday = NSLocalizedString("addNewSchedule.shortThuesday", comment: "")
+    let addNewScheduleShortWednesday = NSLocalizedString("addNewSchedule.shortWednesday", comment: "")
+    let addNewScheduleShortThursday = NSLocalizedString("addNewSchedule.shortThursday", comment: "")
+    let addNewScheduleShortFriday = NSLocalizedString("addNewSchedule.shortFriday", comment: "")
+    let addNewScheduleShortSaturday = NSLocalizedString("addNewSchedule.shortSaturday", comment: "")
+    let addNewScheduleShortSunday = NSLocalizedString("addNewSchedule.shortSunday", comment: "")
+    
+}
